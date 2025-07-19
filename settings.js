32b0f1e242174500baae53498f3f99307f702bcb
princebaye223 <herokubotndao@gmail.com>
2025-07-19 15:11:42 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;TQNznQQY#en_b_ActtxSQqbcPHkaQoV79f-ql64WfcGPFICVxv5w", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "221752512410", // Put Owner Number Here 
+  PREFIX: "," // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
